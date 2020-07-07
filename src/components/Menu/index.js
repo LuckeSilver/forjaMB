a96c48cfff865cf52a0ed17b39f1941792be94c4
Lucas Silva <jacks2kar@gmail.com>
2020-07-07 09:26:21 -0300

    primeiro commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+import "./styles.css";
+
+function Menu() {
+  return (
+    <header className="navigation">
+      <input type="checkbox" id="check" />
+      <label htmlFor="check">
+        <GiHamburgerMenu size={35} />
+      </label>
+      <nav className="menu">
+        <Link className="linkMenu" to="/">
+          Principal
+          <hr />
+        </Link>
+        <Link className="linkMenu" to="/evolucao">
+          Evolução
+          <hr />
+        </Link>
+        <Link className="linkMenu" to="/habilidades">
+          Habilidades
+          <hr />
+        </Link>
+        <Link className="linkMenu" to="/equipamentos">
+          Equipamentos
+          <hr />
+        </Link>
+        <Link className="linkMenu" to="/companheiros">
+          Companheiros
+          <hr />
+        </Link>
+        <Link className="linkMenu" to="/export">
+          Exportar
+          <hr />
+        </Link>
+        <Link className="linkMenu" to="/">
+          Salvar
+          <hr />
+        </Link>
+        <Link className="linkMenu" to="/">
+          Voltar
+          <hr />
+        </Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Menu;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Menu from "../../components/Menu";
+import "./styles.css";
+
+function Home() {
+  return (
+    <>
+      <Menu />
+    </>
+  );
+}
+
+export default Home;
