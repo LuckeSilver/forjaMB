ba1c543018dcc470bba0f68a6410fc345e82d109
Lucas Silva <jacks2kar@gmail.com>
2020-07-07 09:40:36 -0300

    menu responsivo

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -6,44 +6,46 @@ import "./styles.css";
 
 function Menu() {
   return (
-    <header className="navigation">
+    <header>
       <input type="checkbox" id="check" />
       <label htmlFor="check">
         <GiHamburgerMenu size={35} />
       </label>
       <nav className="menu">
-        <Link className="linkMenu" to="/">
-          Principal
-          <hr />
-        </Link>
-        <Link className="linkMenu" to="/evolucao">
-          Evolução
-          <hr />
-        </Link>
-        <Link className="linkMenu" to="/habilidades">
-          Habilidades
-          <hr />
-        </Link>
-        <Link className="linkMenu" to="/equipamentos">
-          Equipamentos
-          <hr />
-        </Link>
-        <Link className="linkMenu" to="/companheiros">
-          Companheiros
-          <hr />
-        </Link>
-        <Link className="linkMenu" to="/export">
-          Exportar
-          <hr />
-        </Link>
-        <Link className="linkMenu" to="/">
-          Salvar
-          <hr />
-        </Link>
-        <Link className="linkMenu" to="/">
-          Voltar
-          <hr />
-        </Link>
+        <div className="navigation">
+          <Link className="linkMenu" to="/">
+            Principal
+            <hr />
+          </Link>
+          <Link className="linkMenu" to="/evolucao">
+            Evolução
+            <hr />
+          </Link>
+          <Link className="linkMenu" to="/habilidades">
+            Habilidades
+            <hr />
+          </Link>
+          <Link className="linkMenu" to="/equipamentos">
+            Equipamentos
+            <hr />
+          </Link>
+          <Link className="linkMenu" to="/companheiros">
+            Companheiros
+            <hr />
+          </Link>
+          <Link className="linkMenu" to="/export">
+            Exportar
+            <hr />
+          </Link>
+          <Link className="linkMenu" to="/">
+            Salvar
+            <hr />
+          </Link>
+          <Link className="linkMenu" to="/">
+            Voltar
+            <hr />
+          </Link>
+        </div>
       </nav>
     </header>
   );
